9201b1206224ff68478f54fe3e4bf4bb1fa03edc
phasi <koumiyom4@gmail,com>
2022-02-12 00:18:43 +0900

    made until sending prepa dates

--- src/line_bot.js
+++ src/line_bot.js
@@ -1,4 +1,4 @@
-import { ACCESSTOKEN } from "./api";
+// import { ACCESSTOKEN } from "./api";
 
 //コマンドの用意
 var command = {
@@ -9,6 +9,7 @@ var command = {
 };
 
 var today = new Date();
+var date = today.getDate();
 var month = today.getMonth() + 1;
 var next_month = month + 1;
 // WebHookで受信した応答用Token
@@ -24,6 +25,12 @@ var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 var getsheet = function (sheet_name) {
   return spreadsheet.getSheetByName(sheet_name);
 };
+
+var sheet_prepa = getsheet("プレパ日程");
+var sheet_prepa_lastrow = sheet_prepa.getLastRow();
+var sheet_prepa_lastcolumn = sheet_prepa.getLastColumn();
+
+var sheet_send = getsheet("任意メッセージ");
 
 // LINE developersのメッセージ送受信設定に記載のアクセストークン
 var ACCESS_TOKEN = ACCESSTOKEN;
@@ -50,10 +57,27 @@ function doPost(e) {
   ).setMimeType(ContentService.MimeType.JSON);
 }
 
-var fetchPrepaarea = function () {};
-var fetchPrepas = function () {};
-var mach_command = function (msg) {
+var fetchPrepaarea = function (sheet, lastRow, lastColumn) {
+  var range = sheet.getRange(`R2C1:R${lastRow}C${lastColumn}`);
+  var data = range.getValues();
+  return data;
+};
+var make_text = function (data) {};
+var mach_command = function (msg, data) {
   var text = "";
   if (msg.search(command.show) >= 0) {
+    for (let i = 0; i < data.length; i++) {
+      text += `${data[i][0]}月${data[i][1]}日  ${data[i][2]}~  ${data[i][3]}教室\n`;
+    }
+    text += "で行ってます！";
+  } else if (msg.search(command.help) >= 0) {
+    text =
+      "コマンド\n#prepas:1か月先までのプレパ日程に表示\n#retell:直近プレパの再確認\n#attend:出席登録（comming soon）";
+  } else if (msg.search(command.attend) >= 0) {
+    text = "comming soon";
+  } else if (msg.search(command.again) >= 0) {
+    text = kokuchi;
   }
 };
+
+function sendByButton() {}
