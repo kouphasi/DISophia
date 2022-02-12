a8b4d373816016a13e703861b2f9d8855efbd1dd
phasi <koumiyom4@gmail,com>
2022-02-12 17:57:29 +0900

    made well

--- src/line_bot.js
+++ src/line_bot.js
@@ -26,11 +26,10 @@ var sheet_prepa = getsheet("プレパ日程");
 var sheet_prepa_lastrow = sheet_prepa.getLastRow();
 var sheet_prepa_lastcolumn = sheet_prepa.getLastColumn();
 
-var sheet_data = fetchPrepaarea(
-  sheet_prepa,
-  sheet_prepa_lastrow,
-  sheet_prepa_lastcolumn
+var range = sheet_prepa.getRange(
+  `R2C1:R${sheet_prepa_lastrow}C${sheet_prepa_lastcolumn}`
 );
+var data = range.getValues();
 
 // LINE developersのメッセージ送受信設定に記載のアクセストークン
 var ACCESS_TOKEN = "";
@@ -41,7 +40,25 @@ function doPost(e) {
   // ユーザーのメッセージを取得
   var userMessage = JSON.parse(e.postData.contents).events[0].message.text;
 
-  var botMessage = mach_command(userMessage, sheet_data);
+  var botMessage = "";
+
+  if (userMessage.search(command.show) >= 0) {
+    console.log("get prepas");
+    botMessage += "直近二か月のプレパは、\n";
+    for (let i = 0; i < data.length; i++) {
+      if (data[i][0] >= today_month && data[i][0] <= next_month) {
+        botMessage += `${data[i][0]}月${data[i][1]}日  ${data[i][2]}~  ${data[i][3]}教室\n`;
+      }
+    }
+    botMessage += "で行います！";
+  } else if (userMessage.search(command.help) >= 0) {
+    console.log("get helpme");
+    botMessage =
+      "コマンド\n#prepas:直近2か月のプレパ日程に表示\n#retell:直近プレパの再確認\n#attend:出席登録（comming soon）";
+  } else if (userMessage.search(command.attend) >= 0) {
+    console.log("get attend");
+    botMessage = "comming soon";
+  }
 
   UrlFetchApp.fetch(url, {
     headers: {
@@ -63,30 +80,3 @@ function doPost(e) {
     JSON.stringify({ content: "post ok" })
   ).setMimeType(ContentService.MimeType.JSON);
 }
-
-var fetchPrepaarea = function (sheet, lastRow, lastColumn) {
-  var range = sheet.getRange(`R2C1:R${lastRow}C${lastColumn}`);
-  var data = range.getValues();
-  return data;
-};
-
-var mach_command = function (msg, data) {
-  var text = "";
-  if (msg.search(command.show) >= 0) {
-    text += "直近二か月のプレパは、\n";
-    for (let i = 0; i < data.length; i++) {
-      if (data[i][0] >= today_month && data[i][0] <= next_month) {
-        text += `${data[i][0]}月${data[i][1]}日  ${data[i][2]}~  ${data[i][3]}教室\n`;
-      }
-    }
-    text += "で行います！";
-  } else if (msg.search(command.help) >= 0) {
-    text =
-      "コマンド\n#prepas:直近2か月のプレパ日程に表示\n#retell:直近プレパの再確認\n#attend:出席登録（comming soon）";
-  } else if (msg.search(command.attend) >= 0) {
-    text = "comming soon";
-  }
-  return text;
-};
-
-function sendByButton() {}
